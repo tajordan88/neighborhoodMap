78972ae7545a61fd2d87bacddff7832907ddd1d3
Jordan Ta <tajordan88@gmail.com>
2017-02-07 19:38:46 -0800

    Cleanup Code.

--- map.js
+++ map.js
@@ -1,29 +1,27 @@
 // Create a map variable
 var map,
-    infowindow;
+  infowindow;
 
 // Create a new blank array for all the listing markers.
 var markers = [];
 
 // Locations. Add more here if you want more locations!
 var locations = [
-  {title: 'American University', location: {lat: 38.937352, lng: -77.088447}, category: 'Education'},
-  {title: 'Georgetown University', location: {lat: 38.907567, lng: -77.072269}, category: 'Education'},
-  {title: 'George Washington University', location: {lat: 38.899698, lng: -77.048567}, category: 'Education'},
-  {title: 'The White House', location: {lat: 38.897591, lng: -77.036598}, category: 'Attraction'},
-  {title: 'Lincoln Memorial', location: {lat: 38.889320, lng: -77.050089}, category: 'Attraction'},
+  { title: 'American University', location: { lat: 38.937352, lng: -77.088447 }, category: 'Education' },
+  { title: 'Georgetown University', location: { lat: 38.907567, lng: -77.072269 }, category: 'Education' },
+  { title: 'George Washington University', location: { lat: 38.899698, lng: -77.048567 }, category: 'Education' },
+  { title: 'The White House', location: { lat: 38.897591, lng: -77.036598 }, category: 'Attraction' },
+  { title: 'Lincoln Memorial', location: { lat: 38.889320, lng: -77.050089 }, category: 'Attraction' },
 ];
-
-
 
 
 // Function to initialize the map within the map div
 function initMap() {
   // Props to https://snazzymaps.com/style/89310/vraplha for the map styling!
-  var styles = [{"featureType":"all","elementType":"geometry","stylers":[{"color":"#1d2c4d"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#8ec3b9"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#1a3646"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#0e1626"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"color":"#4b6878"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"color":"#4b6878"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#64779e"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#25374b"}]},{"featureType":"landscape.man_made","elementType":"geometry.stroke","stylers":[{"color":"#334e87"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#283e55"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#25374b"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#6f9ba5"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"color":"#1d2c4d"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#2b455e"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#3c7680"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#162832"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#98a5be"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#1d2c4d"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#1175ba"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0d5a8e"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#b0d5ce"}]},{"featureType":"road.highway","elementType":"labels.text.stroke","stylers":[{"color":"#2b455e"}]},{"featureType":"transit","elementType":"labels.text.fill","stylers":[{"color":"#98a5be"}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"color":"#1d2c4d"}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"color":"#162832"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#283c51"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#0e1626"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#4e6d70"}]}];
+  var styles = [{ "featureType": "all", "elementType": "geometry", "stylers": [{ "color": "#1d2c4d" }] }, { "featureType": "all", "elementType": "labels.text.fill", "stylers": [{ "color": "#8ec3b9" }] }, { "featureType": "all", "elementType": "labels.text.stroke", "stylers": [{ "color": "#1a3646" }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#0e1626" }] }, { "featureType": "administrative.country", "elementType": "geometry.stroke", "stylers": [{ "color": "#4b6878" }] }, { "featureType": "administrative.province", "elementType": "geometry.stroke", "stylers": [{ "color": "#4b6878" }] }, { "featureType": "administrative.land_parcel", "elementType": "labels.text.fill", "stylers": [{ "color": "#64779e" }] }, { "featureType": "landscape", "elementType": "geometry.fill", "stylers": [{ "color": "#25374b" }] }, { "featureType": "landscape.man_made", "elementType": "geometry.stroke", "stylers": [{ "color": "#334e87" }] }, { "featureType": "landscape.natural", "elementType": "geometry", "stylers": [{ "color": "#283e55" }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#25374b" }] }, { "featureType": "poi", "elementType": "labels.text.fill", "stylers": [{ "color": "#6f9ba5" }] }, { "featureType": "poi", "elementType": "labels.text.stroke", "stylers": [{ "color": "#1d2c4d" }] }, { "featureType": "poi.park", "elementType": "geometry.fill", "stylers": [{ "color": "#2b455e" }] }, { "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [{ "color": "#3c7680" }] }, { "featureType": "road", "elementType": "geometry", "stylers": [{ "color": "#162832" }] }, { "featureType": "road", "elementType": "labels.text.fill", "stylers": [{ "color": "#98a5be" }] }, { "featureType": "road", "elementType": "labels.text.stroke", "stylers": [{ "color": "#1d2c4d" }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "color": "#1175ba" }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#0d5a8e" }] }, { "featureType": "road.highway", "elementType": "labels.text.fill", "stylers": [{ "color": "#b0d5ce" }] }, { "featureType": "road.highway", "elementType": "labels.text.stroke", "stylers": [{ "color": "#2b455e" }] }, { "featureType": "transit", "elementType": "labels.text.fill", "stylers": [{ "color": "#98a5be" }] }, { "featureType": "transit", "elementType": "labels.text.stroke", "stylers": [{ "color": "#1d2c4d" }] }, { "featureType": "transit.line", "elementType": "geometry.fill", "stylers": [{ "color": "#162832" }] }, { "featureType": "transit.station", "elementType": "geometry", "stylers": [{ "color": "#283c51" }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#0e1626" }] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [{ "color": "#4e6d70" }] }];
 
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.74135, lng: -73.99802},
+    center: { lat: 40.74135, lng: -73.99802 },
     zoom: 14,
     styles: styles
   });
@@ -53,93 +51,40 @@ function initMap() {
     markers.push(marker);
 
     vm.locationList()[i].marker = marker;
-    // Extend the boundaries of hte map for each marker.
+    // Extend the boundaries of the map for each marker.
     bounds.extend(marker.position);
-
-
-
 
 
     // Create an onlick event to open an infowindow at each marker.
     marker.addListener('click', function() {
-      // populateInfoWindow(this, largeInfowindow);
-
       var markerNum = this.id;
-      var lastMarkerNum = markers[markerNum];
       getWikiData(this.title, this);
-      // console.log(markerNum);
       toggleBounce(markerNum);
-      // console.log(this.position.lat);
-      // var latLng = {lat: 38.937352, lng: -77.088447};
-      console.log(this);
-      // map.panTo(latLng);
-
     });
-
-    // Bounce Animation On Click
-    // marker.addListener('click', toggleBounce);
-
-
-
-
-
   }
 
 
-    // Function for toggling Bounce on click of Marker
-    console.log(marker);
-    function toggleBounce(markerNum) {
-      var markerHolder = markerNum;
-      markers[markerNum].setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){ markers[markerNum].setAnimation(null); }, 750);
-    }
-
-
+  // Function for toggling Bounce on click of Marker
+  function toggleBounce(markerNum) {
+    markers[markerNum].setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function() { markers[markerNum].setAnimation(null); }, 750);
+  }
 
 }
-
-
-
 
 
 // Get information from WIKIPEDIA
 function getWikiData(title, clickobjectthis) {
 
   // Wikipedia AJAX request
-  // console.log(title);
   var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + title + '&format=json&callback=wikiCallback';
-
-
   $.ajax({
     url: wikiURL,
     dataType: 'jsonp',
-    // jsonp: "callback",
-    success: function( response ) {
-      var wikiName = response[1][0];
+    success: function(response) {
       var wikiGeneralInfo = response[2][0];
       var infoInsideInfoWindow = wikiGeneralInfo;
-
-      // console.log(response);
-      // console.log(response[1]);
-      // console.log(response[2][0]);
-      // console.log(clickobjectthis);
-      // console.log(infoInsideInfoWindow);
-
-      // var largeInfowindow = google.maps.InfoWindow({
-      //   content: infoInsideInfoWindow
-      // });
-
-
-
-
       populateInfoWindow(clickobjectthis, infoInsideInfoWindow);
-
-
-
-      // for (var i = 0; i < articleList.length; i++) {
-      //   articleStr = articleList[i];
-      //   var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-      // };
     }
   });
 
@@ -193,15 +138,15 @@ var ViewModel = function(loc) {
   // Generates list of locations
   this.locationList = ko.observableArray([]);
 
-  locations.forEach(function(locItem){
-    self.locationList.push( new Loc(locItem) );
+  locations.forEach(function(locItem) {
+    self.locationList.push(new Loc(locItem));
   });
 
-  this.currentLoc = ko.observable( this.locationList()[0] );
+  this.currentLoc = ko.observable(this.locationList()[0]);
 
   this.viewLoc = function(clickedLoc) {
     self.currentLoc(clickedLoc);
-    getWikiData(clickedLoc.marker.title,clickedLoc.marker);
+    getWikiData(clickedLoc.marker.title, clickedLoc.marker);
   };
 
 
@@ -212,16 +157,9 @@ var ViewModel = function(loc) {
   // Filter functionality for dropdown sidelist.
   this.filter = ko.computed(function() {
     var myChosenCategory = self.myChosenCategory().toLowerCase();
-    // console.log("self.myChosenCategory(): " + self.myChosenCategory());
     var newArray = ko.utils.arrayFilter(self.locationList(), function(location) {
-      // console.log(location);
       var category = location.category.toLowerCase();
-      // console.log("category: " + category);
-      // console.log("myChosenCategory: " + myChosenCategory);
       var hasCategory = (category === myChosenCategory) || (myChosenCategory === 'all'); // true or false
-      // console.log("hasCategory: " + hasCategory);
-      // console.log("location, myChosenCategory, hasCategory: " + location, myChosenCategory, hasCategory);
-      //return hasCategory; // true or false
 
       if (hasCategory) {
         // show the marker
@@ -245,25 +183,7 @@ var vm = new ViewModel();
 ko.applyBindings(vm); // This makes Knockout get to work
 
 
-
-
-
 // Functionality for Toggling Sidebar
-// $(document).ready(function(){
-// $('#toggle-nav-button').toggle(
-//   function() {
-//     $('#sidebar-wrapper').css('width', '270px')
-//   }, function() {
-//     $('#sidebar-wrapper').css('width', '0')
-// });
-// });
-
-$('#toggle-nav-button').on('click', function () {
-    $("#sidebar-wrapper").toggleClass('sidebar-show')
+$('#toggle-nav-button').on('click', function() {
+  $("#sidebar-wrapper").toggleClass('sidebar-show');
 });
-
-
-
-
-
-
